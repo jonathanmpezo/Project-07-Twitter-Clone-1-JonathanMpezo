cd10e422f0899b4b362028d9a607b8c73ddac7f5
jonathanmpezo <140553166+jonathanmpezo@users.noreply.github.com>
2024-03-12 20:40:22 +0000

    collon droite

--- src/components/trends.jsx
+++ src/components/trends.jsx
@@ -11,9 +11,23 @@ function Trends(){
     <table>
       <section className="trends">
         <div class="flex-1 min-w-0">
-          <div className="rosse">
+          <div className="">
             <div class="ax-w-md space-y-1 text-gray-100 list-none list-inside dark:text-gray-400">
-              <tbody>
+              <div className="masse">
+                <form class="max-w-md mx-auto">   
+                    {/* <label for="default-search" class="mb-2 text-sm font-medium text-gray-100 sr-only dark:text-white">Search</label> */}
+                    <div class="relative">
+                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                            </svg>
+                        </div>
+                        <input type="search" id="default-search" class="block w-full p-1 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+
+                    </div>
+                </form>
+              </div>
+              <tbody className="rosse">
                 {/* <div class="relative">
                 
                   <div className="">
@@ -31,35 +45,16 @@ function Trends(){
 
                   
                 </div> */}
-                <div>
-                  <form class="max-w-md mx-auto">   
-                      {/* <label for="default-search" class="mb-2 text-sm font-medium text-gray-100 sr-only dark:text-white">Search</label> */}
-                      <div class="relative">
-                          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                              <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                              </svg>
-                          </div>
-                          <input type="search" id="default-search" class="block w-full p-1 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
-                         
-                      </div>
-                  </form>
-                </div>
-               
-
-                   
-                <div>
+                <div className= "" >
                   <div>
                     <tr>
                        <th scope="col">Trends for you </th>
-                      <th><td> <img src={kat}/></td></th>
+                      <td> <img src={kat}/></td>
                     </tr>
                   </div>
                 </div>
                
-
-
-                 <div>
+               <div>
                    <p class="text-sm text-gray-500 truncate dark:text-gray-400"> Trending in turkey </p><img src={pic}/> <th scope="col">#SQUI <p class="text-sm text-gray-500 truncate dark:text-gray-400"> 2,066 Tweets </p></th>
                  </div> 
 
